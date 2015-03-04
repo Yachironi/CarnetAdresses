da084736aa4b75ddf6185dd563e0e20bdd795802
Yasser RABI <yachironi@hotmail.com>
2015-03-05 00:49:55 +0100

    updates

--- src/update.php
+++ src/update.php
@@ -1,13 +1,28 @@
 <?php
-    // Lecture de la base de données
-    $db = mysql_connect('127.0.0.1', 'adminQcfpaGS', '9-TQXvf-48mW'); // Connection à la base de données
+       // Lecture de la base de données
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
+    $db = mysql_connect(DB_HOST, DB_USER,DB_PASS ); // Connection à la base de données
     mysql_select_db('carnetadresses',$db);  // Spécification du nom de la base de données
     $sql = "SELECT * FROM `adresse` ORDER BY  `adresse`.`nom` ASC;"; // Requête SQL qui donne les adresse ordonnée par les noms
     $result = mysql_query($sql);  // Envoie de la requête au serveur de bose de donnée
-    $rows = array(); // Initialisation du variable des résultats 
-   while($r = mysql_fetch_assoc($result)) { // Parcours des résultats donnée par la 
+    $rows = array(); // Initialisation du variable des résultats
+$i=0;
+   while($r = mysql_fetch_assoc($result)) { // Parcours des résultats donnée par la
      $rows[] = $r;
+echo $i+++"  ";
    }
-    echo '{"adresses":'.json_encode($rows).'}'; // Retour du résultat en format JSON
+if($db){
+echo '{"adresses":'.json_encode($rows).'}'; // Retour du résultat en format JSON
+}
+if($result){
+echo "Erreur dans la rêquete";
+} 
+if{
+echo "Erreur de connection à la base";}
     mysql_close($db); // Fermeture de la connection ouverte avec le serveur de base de données
 ?>
