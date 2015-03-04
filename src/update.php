84a4f05aba4f32cb9fb40e903c2082b3ebe7f098
Yasser RABI <yachironi@hotmail.com>
2015-03-05 00:59:07 +0100

    .

--- src/update.php
+++ src/update.php
@@ -1,26 +1,13 @@
 <?php
        // Lecture de la base de données
-echo "here 1";
     $db = mysql_connect('127.9.143.130','adminQcfpaGS','9-TQXvf-48mW'); // Connection à la base de données
     mysql_select_db('carnetadresses',$db);  // Spécification du nom de la base de données
     $sql = "SELECT * FROM `adresse` ORDER BY  `adresse`.`nom` ASC;"; // Requête SQL qui donne les adresse ordonnée par les noms
     $result = mysql_query($sql);  // Envoie de la requête au serveur de bose de donnée
     $rows = array(); // Initialisation du variable des résultats
-$i=0;
-echo "here 2";
    while($r = mysql_fetch_assoc($result)) { // Parcours des résultats donnée par la
      $rows[] = $r;
-echo $i+++"  ";
    }
-echo "here 3";
-if($db){
-//echo '{"adresses":'.json_encode($rows).'}'; // Retour du résultat en format JSON
-echo "yasser";
-}
-if($result){
-echo "Erreur dans la rêquete";
-} 
-if{
-echo "Erreur de connection à la base";}
+echo '{"adresses":'.json_encode($rows).'}'; // Retour du résultat en format JSON
     mysql_close($db); // Fermeture de la connection ouverte avec le serveur de base de données
 ?>
